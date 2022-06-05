7c9523049a91f8ab94bec9799c0d91107e036fce
Max Hardman <hardmanmax@yahoo.co.uk>
2022-06-05 17:10:50 +0100

    First call & render of API data

--- src/Components/PopularRecipes.js
+++ src/Components/PopularRecipes.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 
 const PopularRecipes = () => {
+  const [popularRecipes, setPopularRecipes] = useState([]);
+
+  const getPopularRecipes = async()=> {
+    const response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=4`);
+    const data = await response.json();
+    //console.log(data.recipes);
+    setPopularRecipes(data.recipes); 
+  };
+
+  useEffect(() => {
+    getPopularRecipes();
+  }, []);
+
   return (
-    <div>PopularRecipes</div>
+    <> 
+      {
+        popularRecipes.map((item) => (
+          <h2 key={item.id}>{item.title}</h2>
+        ))
+      }
+    </>
   )
 }
 
--- src/Routes/Homepage.js
+++ src/Routes/Homepage.js
@@ -1,14 +1,15 @@
 import Searchbar from "../Components/Searchbar";
 import PopularRecipes from "../Components/PopularRecipes";
 import VegetarianRecipes from "../Components/VegetarianRecipes";
+import VeganRecipes from "../Components/VeganRecipes";
 
 const Homepage = () => {
   return (
     <>
-      <div>Homepage</div>
       <Searchbar />
       <PopularRecipes/>
       <VegetarianRecipes/>
+      <VeganRecipes />
     </>
   )
 }
